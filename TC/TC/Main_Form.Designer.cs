e557e1d60d133e0a9328648c1c9c0b05640a17ee
vophucuong <crysis.return@gmail.com>
2019-12-12 15:12:55 +0700

    cường 12/12/19

--- TC/TC/Main_Form.Designer.cs
+++ TC/TC/Main_Form.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace TC
 {
-    partial class Form1
+    partial class Main_Form
     {
         /// <summary>
         /// Required designer variable.
@@ -36,7 +36,8 @@
             // 
             // monthCalendar1
             // 
-            this.monthCalendar1.Location = new System.Drawing.Point(18, 52);
+            this.monthCalendar1.Location = new System.Drawing.Point(14, 42);
+            this.monthCalendar1.Margin = new System.Windows.Forms.Padding(7, 7, 7, 7);
             this.monthCalendar1.MaxSelectionCount = 1;
             this.monthCalendar1.Name = "monthCalendar1";
             this.monthCalendar1.TabIndex = 0;
@@ -44,9 +45,10 @@
             // btnCheck
             // 
             this.btnCheck.Font = new System.Drawing.Font("Tahoma", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnCheck.Location = new System.Drawing.Point(69, 271);
+            this.btnCheck.Location = new System.Drawing.Point(52, 220);
+            this.btnCheck.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnCheck.Name = "btnCheck";
-            this.btnCheck.Size = new System.Drawing.Size(125, 47);
+            this.btnCheck.Size = new System.Drawing.Size(94, 38);
             this.btnCheck.TabIndex = 2;
             this.btnCheck.Text = "Check";
             this.btnCheck.UseVisualStyleBackColor = true;
@@ -54,9 +56,10 @@
             // btnExit
             // 
             this.btnExit.Font = new System.Drawing.Font("Tahoma", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnExit.Location = new System.Drawing.Point(69, 336);
+            this.btnExit.Location = new System.Drawing.Point(52, 273);
+            this.btnExit.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnExit.Name = "btnExit";
-            this.btnExit.Size = new System.Drawing.Size(125, 47);
+            this.btnExit.Size = new System.Drawing.Size(94, 38);
             this.btnExit.TabIndex = 3;
             this.btnExit.Text = "Exit";
             this.btnExit.UseVisualStyleBackColor = true;
@@ -65,24 +68,26 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Tahoma", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(53, 13);
+            this.label1.Location = new System.Drawing.Point(40, 11);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(156, 30);
+            this.label1.Size = new System.Drawing.Size(125, 24);
             this.label1.TabIndex = 1;
             this.label1.Text = "Time Control";
             // 
-            // Form1
+            // Main_Form
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(267, 404);
+            this.ClientSize = new System.Drawing.Size(200, 328);
             this.Controls.Add(this.btnExit);
             this.Controls.Add(this.btnCheck);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.monthCalendar1);
             this.ForeColor = System.Drawing.Color.Crimson;
-            this.Name = "Form1";
-            this.Text = "Form1";
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "Main_Form";
+            this.Text = "TC";
             this.ResumeLayout(false);
             this.PerformLayout();
 
